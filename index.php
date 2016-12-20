6c2fd9fe32b5f1746b91f258f9335eab12f70e57
Cas van 't Wout <cas@bolboosch.nl>
2016-12-20 11:51:57 +0100

    Skeleton fixed

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php include "includes/header.php"?>
+<nav>
+    Hier komt de nav bar
+</nav>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
 
+            <div class="chart chart-left col-md-4">
 
-The content of the document......
+            </div>
+            <div class="chart chart-right col-md-4">
+
+            </div>
+        </div>
+    </div>
+</div>
 
 
 <?php include "includes/footer.php" ?>
