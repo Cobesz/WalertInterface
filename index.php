ccdc38fe51e27f616f228a9de59f8c635569add9
Cas van 't Wout <cas@bolboosch.nl>
2016-12-20 12:20:33 +0100

    Fixed JS order and added a dropdown

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include "includes/header.php"?>
+<?php include "includes/header.php" ?>
 <nav>
     Hier komt de nav bar
 </nav>
@@ -7,9 +7,37 @@
         <div class="col-md-12">
 
             <div class="chart chart-left col-md-4">
-
+                <!--                Grafiekje in css-->
             </div>
             <div class="chart chart-right col-md-4">
+                <div class="innerbox">
+                    <h2>Meest gebruikt:</h2>
+                    <ul>
+                        <li>
+                            <p>Douche:</p>
+                        </li>
+                        <li>
+                            <p>Wastafel:</p>
+                        </li>
+                        <li>
+                            <p>Keukenkraan:</p>
+                        </li>
+                    </ul>
+                </div>
+                <div class="dropdown">
+                    <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        Dropdown
+                        <span class="caret"></span>
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenu1">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li><a href="#">Separated link</a></li>
+                    </ul>
+                </div>
 
             </div>
         </div>
